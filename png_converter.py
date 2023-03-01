fc810fa3ad3b7b51ae3deb58e68ebf4f9a4ba0fc
maiconFedrigo <97191852+maiconFedrigo@users.noreply.github.com>
2023-03-01 14:30:21 -0300

    Update png_converter.py

--- png_converter.py
+++ png_converter.py
@@ -4,12 +4,9 @@ from reportlab.graphics import renderPDF
 import os
 
 def export_svg_as_png(svg_path, png_path):
-	# Convert svg to pdf in memory with svglib+reportlab
-	# directly rendering to png does not support transparency nor scaling
 	drawing = svglib.svg2rlg(path=svg_path)
 	pdf = renderPDF.drawToString(drawing)
 
-	# Open pdf with fitz (pyMuPdf) to convert to PNG
 	doc = fitz.Document(stream=pdf)
 	pix = doc.load_page(0).get_pixmap(alpha=True, dpi=72)
 	pix.save(png_path)
@@ -30,7 +27,5 @@ png_files_names = [folderpath+"png/"+i.replace(".svg",".png") for i in os.listdi
 for svg_file_path,png_file_path in zip(svg_files_path,png_files_names):
 	export_svg_as_png(svg_file_path,png_file_path)
 
-
-# print(png_files_names)
 
 
